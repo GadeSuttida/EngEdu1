89901c2a1a25cfa9e5c48efe6464ebfad575e7df
GadeSuttida <58030239@kmitl.ac.th>
2017-12-14 03:10:13 +0700

    first year semester 2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semeter 1, I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semeter 2, I learn to program in C sharp.");
+        }
+    }
+}
